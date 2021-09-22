efc71282f88d453942311d209e42541ac81ac419
Naser Abdulkadir <naserabdulkadir0@gmail.com>
2021-09-22 14:31:56 -0400

    styled the active text for when you submit your email. it will check on whether the email is valid or not perfectly on the first attempt but not for following attemps. i need to find a way to toggle the two messages better and i will soon

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 "use strict";
-const button = document.querySelector(".button");
-const inputField = document.getElementById("email");
+const button = document.querySelector(".submit-button");
 const errorText = document.querySelector(".error-text");
+const validText = document.querySelector(".valid-text");
+const inputField = document.getElementById("email");
+let validRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-button.addEventListener("click", function () {
-  inputField.classList.toggle("correct-input");
-  errorText.classList.toggle("hidden");
-});
+function mailChecker() {
+  let validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (
+    inputField.value.match(validRegex) &&
+    validText.classList.contains("hidden")
+  ) {
+    validText.classList.remove("hidden");
+    errorText.classList.add("hidden");
+  } else if (
+    !inputField.value.match(validRegex) &&
+    validText.classList.contains("hidden")
+  ) {
+    errorText.classList.remove("hidden");
+    validText.classList.toggle("hidden");
+  }
+}
+
+button.addEventListener("click", mailChecker);
